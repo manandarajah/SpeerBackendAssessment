0ded5c7eacf0985d5b0adcc38d77232981579a7b
Mugiesshan Anandarajah <manandarajah@users.noreply.github.com>
2021-05-09 15:48:13 -0400

    logout button implemented

--- server.js
+++ server.js
@@ -16,6 +16,14 @@ app.use(passport.session());
 
 mongoose.connect(process.env.DBHOST + process.env.DB, {useNewUrlParser: true, useUnifiedTopology: true});
 
+//Stores all data that EJS files will need to keep it organized and allowing us to manipulate data that's necessary rather than all data
+var data = {
+  isLoggedIn: false,
+  errorOccured: false,
+  name: "",
+  dir: "",
+}
+
 const clientSchema = mongoose.Schema({
   username: String,
   password: String,
@@ -29,7 +37,7 @@ const Client = mongoose.model("client", clientSchema);
 app.get('/', function(req, res) {
   console.log("should call ejs");
 
-  res.render('index', {isLoggedIn: false, errorOccured: false});
+  res.render('index', data);
 });
 
 app.post('/', async function(req, res) {
@@ -40,16 +48,28 @@ app.post('/', async function(req, res) {
   var client = await Client.findOne({'username': username, 'password': password}).exec();
 
   if (client != null) {
-    res.render('index', {isLoggedIn: true, errorOccured: false});
+    data.isLoggedIn = true;
+    data.name = client.firstname + " " + client.lastname;
+    data.dir = req.protocol + '://' + req.get('host');
+    res.render('index', data);
   }
 
   else {
-    res.render('index', {isLoggedIn: false, errorOccured: true});
+    data.errorOccured = true;
+    res.render('index', data);
   }
 });
 
 app.post('/logout', function(req, res) {
-  res.render('index', {isLoggedIn: false, errorOccured: false});
+  console.log("Logging out of client account");
+  data = {
+    isLoggedIn: false,
+    errorOccured: false,
+    name: "",
+    dir: "",
+    balance: 0
+  };
+  res.redirect('/');
 });
 
 app.listen(process.env.PORT || 3000, function() {
